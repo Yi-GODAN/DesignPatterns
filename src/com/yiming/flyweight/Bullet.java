06b5f39e9d863ac79af506ebd481919e34f51bda
YiMing <onegodan@qq.com>
2020-09-27 19:14:31 +0800

    设计模式之flyweight

---
+++ src/com/yiming/flyweight/Bullet.java
@@ -0,0 +1,41 @@
+package com.yiming.flyweight;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @Program: DesignPatterns
+ * @Description:
+ * @Author: YiMing
+ * @Created: 2020/09/27 18:57
+ */
+public class Bullet {
+    public UUID id = UUID.randomUUID();
+    boolean living = true;
+
+}
+
+class BulletPool {
+    List<Bullet> bullets = new ArrayList<>();
+    {
+        for (int i = 0; i < 5; i++) bullets.add(new Bullet());
+    }
+
+    public Bullet getBullet() {
+        for (int i = 0; i < bullets.size(); i++) {
+            Bullet b = bullets.get(i);
+            if (!b.living) return b;
+        }
+        return new Bullet();
+    }
+    public static void main(String[] args) {
+        BulletPool bp = new BulletPool();
+
+        for (int i = 0; i < 10; i++) {
+            Bullet b = bp.getBullet();
+            System.out.println(b);
+        }
+    }
+
+}
---
+++ src/com/yiming/flyweight/TestString.java
@@ -0,0 +1,23 @@
+package com.yiming.flyweight;
+
+/**
+ * @Program: DesignPatterns
+ * @Description:
+ * @Author: YiMing
+ * @Created: 2020/09/27 19:04
+ */
+public class TestString {
+
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+        String s3 = new String("abc");
+        String s4 = new String("abc");
+
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s3 == s4);
+        System.out.println(s3.intern() == s1);
+        System.out.println(s3.intern() == s4.intern());
+    }
+}
