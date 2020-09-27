6356ec9e09b7269b90c2982abe0de4c3940f33a2
YiMing <onegodan@qq.com>
2020-09-27 19:10:54 +0800

    设计模式之Decorator

---
+++ src/com/yiming/Decorator/Main.java
@@ -0,0 +1,21 @@
+package com.yiming.Decorator;
+
+import java.io.*;
+
+/**
+ * @Program: DesignPatterns
+ * @Description:
+ * @Author: YiMing
+ * @Created: 2020/09/27 19:09
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File f = new File("c:/work/test.data");
+        FileOutputStream fos = new FileOutputStream(f);
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+        bw.write("hello");
+        bw.flush();
+        bw.close();
+    }
+}
