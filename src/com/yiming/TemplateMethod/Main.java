adcec112447116a8213aca7e5aaf85a79e290aeb
YiMing <onegodan@qq.com>
2020-09-29 10:54:55 +0800

    设计模式之templatemethod

---
+++ src/com/yiming/TemplateMethod/Main.java
@@ -0,0 +1,39 @@
+package com.yiming.TemplateMethod;
+
+/**
+ * @Program: DesignPatterns
+ * @Description:
+ * @Author: YiMing
+ * @Created: 2020/09/29 10:48
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        F f = new C1();
+        f.m();
+    }
+}
+
+abstract class F {
+    void m() {
+        op1();
+        op2();
+    }
+
+    abstract void op1();
+
+    abstract void op2();
+}
+
+class C1 extends F {
+
+    @Override
+    void op1() {
+        System.out.println("op1");
+    }
+
+    @Override
+    void op2() {
+        System.out.println("op2");
+    }
+}
