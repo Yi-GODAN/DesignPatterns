d52d1ca35a55a3eea8225ee063987b5fed145423
YiMing <onegodan@qq.com>
2022-01-20 22:44:06 +0800

    设计模式之Composite

--- src/com/yiming/decorator/Main.java
+++ src/com/yiming/decorator/Main.java
@@ -10,7 +10,7 @@ import java.io.*;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        File f = new File("c:/work/test.data");
+        File f = new File("d:/work/test.data");
         FileOutputStream fos = new FileOutputStream(f);
         OutputStreamWriter osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
